e14e5b5157e08d8b5ef99f7e6717ec6a5618b230
Waalito <waalo.estudio@gmail.com>
2025-11-11 15:22:46 -0400

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,22 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   experimental: {
     //svg: {
       //mode: 'sprite',
     //}
   },
+  site: 'https://mavfest-uney.netlify.app',
+  integrations: [sitemap({
+    filter: (page) => 
+      page !== "https://mavfest-uney.netlify.app/nominados/",
+  })],
 });
